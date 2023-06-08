aea4811969eb09b29a1b9d8fc94c477c6756b934
JosephApaza <joseph.apaza@ucsm.edu.pe>
2023-06-08 14:38:08 -0500

    Ejercicio aplicando regresión logística.

--- ejercicio.py
+++ ejercicio.py
@@ -5,7 +5,10 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score, roc_auc_score, classification_report
+from sklearn.model_selection import cross_val_score
+import matplotlib.pyplot as plt
+from sklearn.metrics import precision_recall_curve
 
 # Cargar los datos desde el archivo CSV con el separador correcto
 data = pd.read_csv("divorcios.csv", delimiter=";")
@@ -30,8 +33,6 @@ y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print("Precisión del modelo:", accuracy)
 
-from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
-
 # Calcular la matriz de confusión
 confusion = confusion_matrix(y_test, y_pred)
 print("Matriz de confusión:")
@@ -49,3 +50,27 @@ print("Recall:", recall)
 f1 = f1_score(y_test, y_pred)
 print("F1-score:", f1)
 
+# Calcular el AUC-ROC
+y_pred_proba = model.predict_proba(X_test)[:, 1]
+auc_roc = roc_auc_score(y_test, y_pred_proba)
+print("AUC-ROC:", auc_roc)
+
+# Realizar validación cruzada
+cv_scores = cross_val_score(model, X, y, cv=5)
+print("Resultados de validación cruzada:", cv_scores)
+print("Precisión promedio de validación cruzada:", cv_scores.mean())
+
+# Generar el informe de clasificación
+classification_rep = classification_report(y_test, y_pred)
+print("Informe de clasificación:")
+print(classification_rep)
+
+# Calcular la curva de precisión-recall
+precision_curve, recall_curve, _ = precision_recall_curve(y_test, y_pred_proba)
+
+# Graficar la curva de precisión-recall
+plt.plot(recall_curve, precision_curve)
+plt.xlabel("Recall")
+plt.ylabel("Precisión")
+plt.title("Curva de Precisión-Recall")
+plt.show()
